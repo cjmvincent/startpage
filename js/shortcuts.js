3fe0eb3e99a657971d59733e6a7c2a52580b7778
Charles Vincent <cjmvincent@outlook.com>
2025-01-27 13:06:24 -0500

    didnt like it lol

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -38,13 +38,4 @@ export default [
       ChatGPT: "https://chat.openai.com/",
     },
   },
-  {
-    category: "Misc",
-    color: "purple",
-    items: {
-      YouTube: "https://www.youtube.com/",
-      PokemonShowdown: "https://play.pokemonshowdown.com/",
-      ChatGPT: "https://chat.openai.com/",
-    },
-  },
 ];
